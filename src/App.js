ee06d2f8edd36862c21832aeb1f44e6469fe4d2f
rock-zion <rock_zion@outlook.com>
2023-04-03 00:33:28 +0100

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import MyApp from './pages';
+import { Route, Routes } from 'react-router-dom';
+import Overview from './pages/overview';
+import Transactions from './pages/transactions';
+import ErrorPage from './error404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<MyApp />}>
+        <Route index element={<Overview />} />
+        <Route path='transactions' element={<Transactions />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Appbar from 'components/appbar';
+export default function MyApp() {
+  return (
