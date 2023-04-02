051e4b8580b66f491775146b71b54a17363cab4b
rock-zion <rock_zion@outlook.com>
2023-04-03 00:35:47 +0100

    Add language elements and translations

--- src/i18n.js
+++ src/i18n.js
@@ -16,6 +16,115 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+    resources: {
+      en: {
+        translation: {
+          navigation: {
+            dashboard: 'Dashboard',
+            balances: 'Balances',
+            transactions: 'Transactions',
+            analytics: 'Analytics',
+            marketing: 'Marketing',
+            exchange_rates: 'Exchange Rates',
+            checkout: 'Checkout',
+            payment_link: 'Payment Link',
+            wire: 'Wire',
+          },
+          titles: {
+            main_page: 'Main Page',
+            accept_payments: 'Accept Payments',
+            send_payments: 'Send Payments',
+            sales_overview: 'Sales Overview',
+            sales: 'Sales',
+            transaction_history: 'Transaction History',
+          },
+          status: {
+            all_status: 'All Status',
+            failed: 'Failed',
+            in_progress: 'In Progress',
+            success: 'Success',
+            cancelled: 'Cancelled',
+          },
+          // tedo: another one
+          table_titles: {
+            transaction_id: 'Transaction ID',
+            source: 'Source',
+            customer_name: 'Customer name',
+            customer_email: 'Customer email',
+            amount: 'Amount',
+            request_date: 'Request Ddate',
+            status: 'Status',
+          },
+          buttons: {
+            support: 'Support',
+            hide_panel: 'Hide panel',
+            download_report: 'Download report',
+            send_a_wire: 'Send a wire',
+          },
+          marketing:{
+            klasha_wire: "KlashaWire - send money to businesses globally from Africa"
+          },
+          dates: {
+            todays_sales: "Today's sales",
+            this_week: 'This Week',
+            this_month: 'This Month',
+            last_month: 'Last Month',
+          },
+        },
+      },
+
+      fr: {
+        translation: {
+          navigation: {
+            dashboard: 'Tableau de bord',
+            balances: 'Soldes',
+            transactions: 'Transactions',
+            analytics: 'Analytique',
+            marketing: 'Commercialisation',
+            exchange_rates: "Taux d'échange",
+            checkout: 'Vérifier',
+            payment_link: 'Lien de paiement',
+            wire: 'Fil',
+          },
+          titles: {
+            main_page: "Page d'accueil",
+            accept_payments: 'Accepter les paiements',
+            send_payments: 'Envoyer des paiements',
+            sales_overview: 'Aperçu des ventes',
+            sales: 'Ventes',
+            transaction_history: 'Historique des transactions',
+          },
+          status: {
+            all_status: 'Tous les statuts',
+            failed: 'Échoué',
+            in_progress: 'En cours',
+            success: 'Succès',
+            cancelled: 'Annulé',
+          },
+          table_titles: {
+            transaction_id: 'identifiant de transaction',
+            source: 'Source',
+            customer_name: 'Nom du client',
+            customer_email: 'Email client',
+            amount: 'Montante',
+            request_date: 'Date de la demande',
+            status: 'Statut',
+          },
+          buttons: {
+            support: 'Soutien',
+            hide_panel: 'Masquer le panneau',
+            download_report: 'Télécharger le rapport',
+            send_a_wire: 'Envoyer un fil',
+          },
+          marketing:{
+            klasha_wire: "KlashaWire - envoyer de l'argent aux entreprises du monde entier depuis l'Afrique"
+          },
+          dates: {
+            todays_sales: 'Les soldes du jour',
+            this_week: 'Cette semaine',
+            this_month: 'Ce mois-ci',
+            last_month: 'Le mois dernier',
+          },
         },
       },
     },
