cbf7380b362db794c67ba45edf3ab9d18feb2c77
rock-zion <rock_zion@outlook.com>
2023-04-03 00:54:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+import './styles/App.scss';
+import './styles/fonts.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import SidebarContextProvider from 'contexts/sidebar';
+import DateContextProvider from 'contexts/date';
+import './i18n'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DateContextProvider>
+      <SidebarContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SidebarContextProvider>
+    </DateContextProvider>
   </React.StrictMode>
 );
 
