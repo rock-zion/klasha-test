a1e57f8c40105273e0719c3fa671bcf254babab2
rock-zion <rock_zion@outlook.com>
2023-04-03 01:03:27 +0100

    create main sales chart

---
+++ src/components/charts/sales_chart.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
+import zoomPlugin from 'chartjs-plugin-zoom';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  zoomPlugin
+);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      display: true,
+    },
+    y: {
+      display: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+      position: 'top',
+    },
+    title: {
+      display: false,
+    },
+    zoom: {
+      pan: {
+        enabled: true,
+        mode: 'x',
+      },
+      zoom: {
+        pinch: {
+          enabled: true, // Enable pinch zooming
+        },
+        wheel: {
+          enabled: true, // Enable wheel zooming
+        },
+        mode: 'x',
+      },
+    },
+  },
+};
+
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export function SalesChart({ labels = [] }) {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        fill: true,
+        data: labels.map(() => faker.datatype.number({ min: 1000, max: 4000 })),
+        borderWidth: 1,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.4)',
+        pointStyle: 'circle',
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  return <Line options={options} labels={data} data={data} />;
+}
